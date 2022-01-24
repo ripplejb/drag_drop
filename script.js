44f1eb90c89f8eed0a3089c7c1bd34c33244f6d9
ripalbarot <ripplejb@hotmail.com>
2022-01-23 20:13:14 -0600

    fixed the name.

--- script.js
+++ script.js
@@ -31,14 +31,14 @@ function getDragAfterElement(container, y) {
   return draggableElemnts.reduce((closest, child) => {
     const box = child.getBoundingClientRect()
     const offset = y - box.top - box.height / 2
-    if (offset < 0 && offset > closest.off1)
+    if (offset < 0 && offset > closest.offset)
     {
       return {offset: offset, element: child}
     } else
     {
       return closest
     }
-  }, {off1: Number.NEGATIVE_INFINITY}).element // the second parameter is closest for 
+  }, {offset: Number.NEGATIVE_INFINITY}).element // the second parameter is closest for 
                                         // the function in first parameter
 
 }
